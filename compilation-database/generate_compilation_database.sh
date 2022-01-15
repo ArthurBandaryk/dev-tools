d80b962ff685ad2dbc33e9a188289d59203664b3
archi <arthur_bandaryk@mail.ru>
2022-01-15 13:11:59 +0300

    Introduce generating compilation database

---
+++ compilation-database/generate_compilation_database.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+INSTALL_DIR="."
+VERSION="0.5.2"
+
+# Check if `generate.py` is already present.
+which ${INSTALL_DIR}/bazel-compilation-database-${VERSION}/generate.py &> /dev/null || (
+# Download all source files for generating compilation database.
+    cd "${INSTALL_DIR}" \
+    && curl -L "https://github.com/grailbio/bazel-compilation-database/archive/${VERSION}.tar.gz" | tar -xz
+)
+
+# Run python script which generates compilation database.
+# We should add `--action_env=CC=clang` cause we need clang
+# for using clang-tidy.
+${INSTALL_DIR}/bazel-compilation-database-${VERSION}/generate.py -- --action_env=CC=clang
